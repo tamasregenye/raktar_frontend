bedf662783667b73feb754958b31cbe959170c86
Tamas Regenye <tomiregenye@gmail.com>
2026-03-13 09:43:54 +0100

    sonner csomag integrálása

--- src/app/provider.jsx
+++ src/app/provider.jsx
@@ -2,11 +2,13 @@ import { RouterProvider } from "react-router"
 import { router } from "./router";
 import { CategoryProvider } from "../features/category/stores/categoryProvider";
 import { ProductProvider } from "../features/product/stores/productProvider";
+import { Toaster } from "sonner";
 
 export function AppProvider() {
     return (
         <ProductProvider>
             <CategoryProvider>
+                <Toaster richColors position="bottom-right"/>
                 <RouterProvider router={router} />
             </CategoryProvider>
         </ProductProvider>
