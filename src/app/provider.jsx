15578eec14f7c7c16490bb988a8af3a32f66959a
Tamas Regenye <tomiregenye@gmail.com>
2026-03-13 11:39:54 +0100

    AuthProvider

--- src/app/provider.jsx
+++ src/app/provider.jsx
@@ -3,14 +3,17 @@ import { router } from "./router";
 import { CategoryProvider } from "../features/category/stores/categoryProvider";
 import { ProductProvider } from "../features/product/stores/productProvider";
 import { Toaster } from "sonner";
+import { AuthProvider } from "../features/auth/stores/authProvider";
 
 export function AppProvider() {
     return (
-        <ProductProvider>
-            <CategoryProvider>
-                <Toaster richColors position="bottom-right"/>
-                <RouterProvider router={router} />
-            </CategoryProvider>
-        </ProductProvider>
+        <AuthProvider>
+            <ProductProvider>
+                <CategoryProvider>
+                    <Toaster richColors position="bottom-right" />
+                    <RouterProvider router={router} />
+                </CategoryProvider>
+            </ProductProvider>
+        </AuthProvider>
     );
 };
