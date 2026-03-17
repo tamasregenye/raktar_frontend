41bb079badd9040fbd1581ac7868310b525ad740
Tamas Regenye <tomiregenye@gmail.com>
2026-03-17 11:58:28 +0100

    értesítés token lejártáról

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,6 +1,8 @@
 import { Navigate, Outlet, useLocation } from "react-router";
 import { useAuth } from "../features/auth/stores/authProvider";
 import { AccessDeniedPath } from "./AccessDeniedPath";
+import { useEffect } from "react";
+import { toast } from "sonner";
 
 /**
  * 
@@ -8,17 +10,43 @@ import { AccessDeniedPath } from "./AccessDeniedPath";
  * @param {string[]} params.allowedRoles Komponens eléréséhez szükséges szerepkörök - tömbként megadva
  * @returns 
  */
-export function ProtectedRoute ( {allowedRoles} ) {
-    const { token, user} = useAuth();
+export function ProtectedRoute({ allowedRoles }) {
+    const { token, user, logout } = useAuth();
     const location = useLocation();
 
-    if(!token || !user){
-        return <Navigate to="/login"/>
+    useEffect(() => {
+        if (!token || !user || !user.lejarToken) return;
+
+        const handleExpiration = () => {
+            logout();
+            toast.error('Lejárt a munkamenet, jelentkezz be újra!');
+        }
+
+        const expirationTimeInMs = (user.lejarToken * 1000) - Date.now();
+
+        if (expirationTimeInMs <= 0) {
+            handleExpiration();
+        }
+        else {
+            //időzítő indítása, felhasználó kiléptetése a token lejártakor
+            const timer = setTimeout(() => {
+                handleExpiration();
+            }, expirationTimeInMs)
+
+            // korábbi időzítő törlése, ha a felhasználó átkattint egy másik oldalra
+            return () => clearTimeout(timer);
+        }
+
+
+    }, [user, location.pathname])
+
+    if (!token || !user) {
+        return <Navigate to="/login" />
     }
 
-    if(allowedRoles && !allowedRoles.includes(user.szerepkor)){
-        return <AccessDeniedPath/>
+    if (allowedRoles && !allowedRoles.includes(user.szerepkor)) {
+        return <AccessDeniedPath />
     }
 
-    return <Outlet/>
+    return <Outlet />
 }
